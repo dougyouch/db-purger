b639ff2d6545debbcbf69ee64bb315328f683861
Doug Youch <dougyouch@gmail.com>
2019-07-19 17:02:39 -0400

    unsure the entire plan is created successfully

--- spec/db-purger/dynamic_plan_builder_spec.rb
+++ spec/db-purger/dynamic_plan_builder_spec.rb
@@ -8,10 +8,31 @@ describe DBPurger::DynamicPlanBuilder do
   let(:dynamic_plan_builder) { DBPurger::DynamicPlanBuilder.new(database) }
 
   context '#build' do
+    let(:expected_output) do
+<<-STR
+DBPurger::PlanBuilder.build do
+  base_table(:companies, :id)
+
+  parent_table(:employments, :company_id) do
+    child_table(:employment_notes, :employment_id)
+    child_table(:stats_employment_durations, :employment_id)
+  end
+  parent_table(:company_tags, :company_id)
+  parent_table(:stats_company_employments, :company_id)
+
+  child_table(:employments, :company_id) do
+    child_table(:employment_notes, :employment_id)
+    child_table(:stats_employment_durations, :employment_id)
+  end
+  child_table(:company_tags, :company_id)
+  child_table(:stats_company_employments, :company_id)
+end
+STR
+    end
     subject { dynamic_plan_builder.build(base_table_name, field) }
 
     it 'creates a purge plan' do
-      expect(subject.include?('child_table(:employment_notes, :employment_id)')).to eq(true)
+      expect(subject).to eq(expected_output)
     end
   end
 end
